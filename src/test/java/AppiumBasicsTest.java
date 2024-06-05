e2fe31ece180420ed99d3f146afd9cd997068f1b
Alejandra Lopez <alejandralopez8212@gmail.com>
2024-06-04 23:01:02 -0500

    Added wifi settings method and refactor appium startup

--- src/test/java/AppiumBasicsTest.java
+++ src/test/java/AppiumBasicsTest.java
@@ -1,9 +1,19 @@
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class AppiumBasicsTest extends BaseTest {
     @Test
-    public void AppiumTest() {
+    public void WifiSettingsName() {
+        //Activate Wifi
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        //Using Xpath
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+        //Using id
+        driver.findElement(By.id("android:id/checkbox")).click();
+
+
+        //Wifi Settings
+        driver.findElement(By.xpath("//android.widget.TextView[@resource-id='android:id/title']")).click();
     }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -17,15 +17,13 @@ public class BaseTest {
     @BeforeClass
     public void ConfigureAppium() throws MalformedURLException {
         //Starting appium server
-        service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\aleji\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js")).withIPAddress("172.26.80.1").usingPort(4723).build();
+        service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\aleji\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js")).withIPAddress("127.0.0.1").usingPort(4723).build();
         service.start();
-
         //Starting android driver and capabilities
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Pixel 6 Pro AI 33");
+        options.setDeviceName("Pixel 8 Pro API 34");
         options.setApp("C:\\Users\\aleji\\IdeaProjects\\AppiumBasics\\src\\test\\Utils\\ApiDemos-debug.apk");
-        driver = new AndroidDriver(new URL("http://172.26.80.1:4723"), options);
-
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
     }
 
     @AfterClass
